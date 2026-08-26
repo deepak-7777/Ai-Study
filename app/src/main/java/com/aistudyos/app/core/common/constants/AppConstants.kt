1181f9fb859023b8d6fee8e729cec0b71c4ef4a0
deepak <globalxwar76@gmail.com>
2026-08-27 00:20:43 +0530

    first commit

---
+++ app/src/main/java/com/aistudyos/app/core/common/constants/AppConstants.kt
@@ -0,0 +1,22 @@
+package com.aistudyos.app.core.common.constants
+
+object AppConstants {
+    const val PREF_TOKEN         = "pref_access_token"
+    const val PREF_REFRESH_TOKEN = "pref_refresh_token"
+    const val PREF_USER_ID       = "pref_user_id"
+    const val PREF_USER_NAME     = "pref_user_name"
+    const val PREF_USER_EMAIL    = "pref_user_email"
+    const val PREF_ONBOARDING    = "pref_onboarding_done"
+
+    const val DB_NAME            = "ai_study_os_db"
+    const val DB_VERSION         = 1
+
+    const val UPLOAD_WORK_TAG    = "upload_work"
+    const val SYNC_WORK_TAG      = "sync_work"
+
+    const val MAX_FILE_SIZE_MB   = 50L
+    const val PAGE_SIZE          = 20
+
+    const val MIME_PDF           = "application/pdf"
+    const val MIME_AUDIO         = "audio/*"
+}
